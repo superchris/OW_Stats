1212716f5983d0b239fa1f11c0178d407d5f1b2f
Tiko Nelson <tiko.nelson@hotmail.com>
2017-05-26 00:03:10 -0400

    UI and Images

--- app/js/heroPage.js
+++ app/js/heroPage.js
@@ -4,6 +4,8 @@ function heroPage(data){
 		$('#heroAge').html(data.age)
 		$('#heroDetails-p').html(data.description)
 		$('#realName').html(data.real_name)
+		$('#hero-img').empty()
+		$('#hero-img').append('<div><img src="./img/heroes/'+data.name+'.png" width=200px/></div>')
 		$('#difficulty').empty()
 		for (var x=0; x <data.difficulty; x++){
 			$('#difficulty').append('<i class="fa fa-star fa-2x" aria-hidden="true"></i>'+ ' ')
